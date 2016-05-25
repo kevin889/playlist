336a2745cb0f72f910096bc665ed597dfcccc5fa
codedwrench <rick_1996@hotmail.com>
2016-05-26 01:19:34 +0200

    javascript

--- application/controllers/API.php
+++ application/controllers/API.php
@@ -14,4 +14,29 @@ class API extends CI_Controller {
         $output = json_decode($output, true);
         echo json_encode(array_slice($output['data'], 0, 5));
     }
+
+    public function sendToDeezer($playlistid,$data)
+    {
+        $url = "http://api.deezer.com/playlist/". $playlistid ."/tracks";
+        $data = implode(", ",$data);
+        $curl = curl_init($url);
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data));
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        $response = curl_exec($curl);
+        curl_close($curl);
+        return $response;
+    }
+    public function orderDeezer($playlistid,$data)
+    {
+        $url = "http://api.deezer.com/playlist/". $playlistid ."/tracks";
+        $data = implode(", ",$data);
+        $curl = curl_init($url);
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data));
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        $response = curl_exec($curl);
+        curl_close($curl);
+        return $response;
+    }
 }
---
+++ application/views/test.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="nl" class="no-js">
+<head>
+    <meta charset="utf-8">
+    <title>Play Deeze For Me</title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <!--    <link rel='stylesheet' href='style.css'/>-->
+</head>
+<body>
+<div id="player">
+</div>
+<div id="dz-root"></div>
+<script src="http://cdn-files.deezer.com/js/min/dz.js"></script>
+<script>
+
+DZ.init({
+        appId  : '180582',
+        channelUrl : 'http://playdeeze4.me/channel.html',
+        player: {
+            container: 'player',
+            width : 800,
+            height : 300,
+            onload : function(){
+
+                console.log("DZ.init player.onload", arguments);
+
+            }
+        }
+    });
+DZ.ready(function(sdk_options){
+
+    DZ.player.playPlaylist(1861142322);
+});
+
+
+
+</script>
+<input type="button" onclick="DZ.login(function(response) {
+    if (response.authResponse) {
+        console.log('Welcome!  Fetching your information.... ');
+        DZ.api('/user/me', function(response) {
+            console.log('Good to see you, ' + response.name + '.');
+        });
+    } else {
+        console.log('User cancelled login or did not fully authorize.');
+    }
+}, {perms: 'basic_access,email'});" value= "login"</input>
+
+
+</body>
